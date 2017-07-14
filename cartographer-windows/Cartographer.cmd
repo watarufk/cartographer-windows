a354294fae67089e38cebad800696ad95edfbeb8
kishimoto-exvision <kishimoto@exvision.co.jp>
2017-07-14 17:26:11 +0900

    Move cartographer to Projects/cartographer

--- cartographer-windows/Cartographer.cmd
+++ cartographer-windows/Cartographer.cmd
@@ -1,10 +1,10 @@
-REM copy cartographer\CMakeLists.txt ..\cartographer
+REM copy cartographer\CMakeLists.txt ..\Projects\cartographer
 REM call GoogleTest.cmd
 REM call Eigen3.cmd
 
 
 
-cd ..
+cd ..\Projects
 rmdir /S /Q cartographer_build_x64
 mkdir cartographer_build_x64
 
@@ -14,14 +14,14 @@ cd cartographer_build_x64
 
 cmake.exe ../cartographer -G "Visual Studio 14 2015 Win64" ^
 -DCMAKE_SUPPRESS_REGENERATION:BOOL=TRUE ^
--DGTEST_INCLUDE_DIRS:PATH="..\googletest\googletest\include" ^
--DGMOCK_INCLUDE_DIRS:PATH="..\googletest\googlemock\include" ^
--DGMOCK_LIBRARIES:FILEPATH="..\googletest_build_x64\googlemock\Release" ^
--DGMOCK_SRC_DIR:PATH="..\googletest\googlemock\src" ^
--DEIGEN3_INCLUDE_DIR="..\eigen-eigen-5a0156e40feb_build_x64" ^
--DCeres_DIR:PATH="..\ceres-solver-windows\ceres-solver_build_x64" ^
--DCeres_INCLUDE_DIRS:PATH="..\ceres-solver-windows\ceres-solver\include" ^
--DCeres_LIBRARIES:PATH="..\ceres-solver-windows\ceres-solver_build_x64\lib\Release" ^
+-DGTEST_INCLUDE_DIRS:PATH="..\..\googletest\googletest\include" ^
+-DGMOCK_INCLUDE_DIRS:PATH="..\..\googletest\googlemock\include" ^
+-DGMOCK_LIBRARIES:FILEPATH="..\..\googletest_build_x64\googlemock\Release" ^
+-DGMOCK_SRC_DIR:PATH="..\..\googletest\googlemock\src" ^
+-DEIGEN3_INCLUDE_DIR="..\..\ceres-solver-windows\eigen-eigen-5a0156e40feb" ^
+-DCeres_DIR:PATH="..\..\ceres-solver-windows\ceres-solver" ^
+-DCeres_INCLUDE_DIRS:PATH="..\..\ceres-solver-windows\ceres-solver\include" ^
+-DCeres_LIBRARIES:PATH="..\..\ceres-solver-windows\ceres-solver_build_x64\lib\Release" ^
 -DBoost_DIR:PATH="D:/Shared/SDKs/boost_1_64_0" ^
 -DBoost_INCLUDE_DIR:PATH="D:/Shared/SDKs/boost_1_64_0" ^
 -DBoost_LIBRARY_DIR:FILEPATH="D:/Shared/SDKs/boost_1_64_0/lib64-msvc-14.1" ^
@@ -29,21 +29,21 @@ cmake.exe ../cartographer -G "Visual Studio 14 2015 Win64" ^
 -DBoost_IOSTREAMS_LIBRARY_RELEASE:FILEPATH="boost_iostreams-vc141-mt-1_64.lib" ^
 -DBoost_REGEX_LIBRARY_DEBUG:FILEPATH="boost_regex-vc141-mt-gd-1_64.lib" ^
 -DBoost_REGEX_LIBRARY_RELEASE:FILEPATH="boost_regex-vc141-mt-1_64.lib" ^
--DLUA_INCLUDE_PREFIX:PATH="..\lua\src" ^
--DLUA_INCLUDE_DIR="..\lua\src" ^
--DLUA_LIBRARIES="..\lua_build_x64\Release" ^
--DLUA_LIBRARY:FILEPATH="..\lua_build_x64\Release" ^
--DProtobuf_INCLUDE_DIR="..\protobuf\src\google\protobuf" ^
--DProtobuf_SRC_ROOT_FOLDER="..\protobuf\src\google\protobuf" ^
--DProtobuf_LIBRARIES="..\protobuf_cmake_build_x64\Release" ^
+-DLUA_INCLUDE_PREFIX:PATH="..\..\lua\src" ^
+-DLUA_INCLUDE_DIR="..\..\lua\src" ^
+-DLUA_LIBRARIES="..\..\lua_build_x64\Release" ^
+-DLUA_LIBRARY:FILEPATH="..\..\lua_build_x64\Release" ^
+-DProtobuf_INCLUDE_DIR="..\..\protobuf\src\google\protobuf" ^
+-DProtobuf_SRC_ROOT_FOLDER="..\..\protobuf\src\google\protobuf" ^
+-DProtobuf_LIBRARIES="..\..\protobuf_cmake_build_x64\Release" ^
 -DProtobuf_LIBRARY_RELEASE:FILEPATH="libprotobuf.lib" ^
 -DProtobuf_LITE_LIBRARY_RELEASE:FILEPATH="libprotobuf-lite.lib" ^
 -DProtobuf_PROTOC_LIBRARY_RELEASE:FILEPATH="libprotoc.lib" ^
 -DProtobuf_PROTOC_EXECUTABLE:FILEPATH="protoc.exe" ^
--DCAIRO_DIR="..\gtkPlusBuiltFilesForWindows\gtk-build\gtk\x64\" ^
--DCAIRO_INCLUDE_DIRS:PATH="..\gtkPlusBuiltFilesForWindows\gtk-build\gtk\x64\include" ^
--DCAIRO_LIBRARIES:FILEPATH="..\gtkPlusBuiltFilesForWindows\gtk-build\gtk\x64\lib" ^
--DPKG_CONFIG_EXECUTABLE:FILEPATH="../cartographer/configuration_files"
+-DCAIRO_DIR="..\..\gtkPlusBuiltFilesForWindows\gtk-build\gtk\x64\" ^
+-DCAIRO_INCLUDE_DIRS:PATH="..\..\gtkPlusBuiltFilesForWindows\gtk-build\gtk\x64\include" ^
+-DCAIRO_LIBRARIES:FILEPATH="..\..\gtkPlusBuiltFilesForWindows\gtk-build\gtk\x64\lib" ^
+-DPKG_CONFIG_EXECUTABLE:FILEPATH="..\..\cartographer\configuration_files"
 
 REM cmake.exe --build "." --target "ALL_BUILD" --config "Release"
 
@@ -52,5 +52,4 @@ goto :exit
 
 
 :exit
-cd ..
-cd cartographer-windows
+cd "..\..\cartographer-windows"

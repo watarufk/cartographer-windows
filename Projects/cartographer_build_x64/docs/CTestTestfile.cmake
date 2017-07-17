a23ce5f5317cf4ede876aefc0251fcbf974fb1bf
kishimoto-exvision <kishimoto@exvision.co.jp>
2017-07-17 16:24:11 +0900

    Add cmake related files and docs folder generated by cmake on bash on Ubuntu on Windows

---
+++ Projects/cartographer_build_x64/docs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kishimoto/cartographer/docs
+# Build directory: /home/kishimoto/cartographer_build_x64/docs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

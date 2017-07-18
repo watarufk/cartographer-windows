79cc2eb8fce4650defe10a6238f63be3548557ee
kishimoto-exvision <kishimoto@exvision.co.jp>
2017-07-18 18:04:08 +0900

    Update Pthread.cmd
     * pthread.lib is necessary, so copy pthread_lib.lib to pthread.lib.

--- cartographer-windows/Pthread.cmd
+++ cartographer-windows/Pthread.cmd
@@ -12,6 +12,7 @@ msbuild pthread.2015.sln /m /t:Build /p:Configuration=Release;Platform=x64
 cd ..
 REM The last character of the target path must be not "/" but "\".
 xcopy /E /D /Y /Q "pthread-win32"  "%SdksFolderPath%/GerHobbelt/pthread-win32\"
+copy "%SdksFolderPath%\GerHobbelt\pthread-win32\bin\x64_MSVC2015.Release\pthread_lib.lib" "%SdksFolderPath%\GerHobbelt\pthread-win32\bin\x64_MSVC2015.Release\pthread.lib"
 
 
 
